e3d2cf4ece3e93f0249eafa5650edf0236fd3100
MrGaggio <steruggiu@gmail.com>
2021-12-16 15:51:14 +0000

    ricopiato base

--- js/script.js
+++ js/script.js
@@ -4,39 +4,29 @@ Bonus:
 1- al click su uno dei pallini mostrare l’immagine in posizione corrispondente
 2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
-Consigli del giorno:
-Leggete sempre la documentazione, partite dalle cose basilari per poi aggiungere complessità.
-Ricordiamo sempre che è necessario capire bene prima che dati dobbiamo andare a manipolare.
 */
 
 
 const app = new Vue({
     el: '#app',
     data: {
-        //creare un array contenente le immagini
-        images: [
-            "./images/image1.jpg",
-            "./images/image2.jpg",
-            "./images/image3.jpg",
-            "./images/image4.jpg"
-        ],
+      images: [
+          'image1.jpg',
+          'image2.jpg',
+          'image3.jpg',
+          'image4.jpg',
+      ]
 
-        counter: "",
     },
-    methods:{
-        // creare una funzione che al click aggiunge +1 alla foto
-        avanti: function () {
-            // console.log("prova");
-        },
-
-        // creare una funzione che al click sottrae -1 alla foto
-        indietro: function(){
-            // console.log("prova");
+    methods: {
+        //funzione per andare avanti
+        avanti: function(){
 
         }
 
-        // fare ciclo for per trovare l'indice che è uguale al counter se clicco sale l'indice
-
+        //funzione per tornare indietro
+        indietro: function(){
+            
+        }
     }
-
   })
